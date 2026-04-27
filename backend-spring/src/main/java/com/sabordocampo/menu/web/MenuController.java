19b4a17637174e6715ba3d84f87df0f444f818e7
Marcosware <marcosware.dev@gmail.com>
2026-04-26 21:43:27 -0300

    fix(menu): Update CORS config allow 127.0.0.1

--- backend-spring/src/main/java/com/sabordocampo/menu/web/MenuController.java
+++ backend-spring/src/main/java/com/sabordocampo/menu/web/MenuController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
 public class MenuController {
     private final MenuService menuService;
 
